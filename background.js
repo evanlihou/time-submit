03bdfb9a213eb7e580df4b22198646003f7f55ae
Evan Lihou <evan@evanlihou.com>
2019-03-14 13:32:34 -0400

    Adds error checking

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const TIMESHEET_URL = "https://rest.tsheets.com/api/v1/timesheets";
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function(request, _sender, sendResponse) {
         if (request.getTimesheet) {
             var req = request.getTimesheet
             chrome.storage.sync.get({
@@ -9,6 +9,14 @@ chrome.runtime.onMessage.addListener(
                 tsheets_job_id: '',
                 tsheets_user_id: ''
             }, (items) => {
+                if (!items.tsheets_token || !items.tsheets_job_id || !items.tsheets_user_id) {
+                    sendResponse({
+                        error: {
+                            message: 'One or more configuration values were empty. Please ensure extension options are set.'
+                        }
+                    })
+                    return;
+                }
                 var params = {
                     start_date: req.start,
                     end_date: req.end,
@@ -21,6 +29,14 @@ chrome.runtime.onMessage.addListener(
                 }).then(res => res.json())
                 .then(response => {
                     console.log(response);
+                    if (!response.results) {
+                        sendResponse({
+                            error: {
+                                message: 'Received invalid response from TSheets.' + (response.error ? ' Error: ' + response.error.message : '')
+                            }
+                        })
+                        return;
+                    }
                     var tsData = response.results.timesheets;
                     var dateTotals = {};
                     for (var key in tsData) {
@@ -31,10 +47,15 @@ chrome.runtime.onMessage.addListener(
                                 comments: []
                             };
                         }
-                        dateTotals[value.date].seconds += value.duration;
-                        dateTotals[value.date].comments.push(value.notes);
+                        if (value.on_the_clock) {
+                            // Hack: Currently assumes user's timezone is the same as the one on the timesheet. This might also break with daylight savings transitions.
+                            var secondsDifference = (new Date() - new Date(value.start)) / 1000
+                            dateTotals[value.date].seconds += secondsDifference;
+                        } else {
+                            dateTotals[value.date].seconds += value.duration;
+                        }
+                        if (value.notes !== "") dateTotals[value.date].comments.push(value.notes);
                     }
-                    console.log('HANDLE',dateTotals);
                     sendResponse(dateTotals);
                 });
             });
--- content.js
+++ content.js
@@ -1,7 +1,4 @@
 function fillTime() {
-    document.querySelectorAll(".timesheets").forEach(function (el, i, arr) {
-        el.classList.remove('hide-weekends');
-    });
     const weekStr = document.getElementsByName("data[week_no]")[0].value;
     const week = {
         weekNo: weekStr.substring(4,6),
@@ -14,16 +11,24 @@ function fillTime() {
         start: weekBounds.start.toISODateString(),
         end: weekBounds.end.toISODateString()
     }}, function (response) {
-        for (var day in response) {
-            var dayIndex = new Date(day + ' EDT').getDay(); // TZ Hack
-            var el = document.querySelector('input.time.regular[data-day="' + dayIndex + '"]');
-            var comments = response[day].comments.join('\n');
-            el.value = Math.round(response[day].seconds / 60 / 60 * 10)/10;
-            el.title = comments;
-            // Trigger onchange for sums
-            var evt = new Event('change');
-            el.dispatchEvent(evt);
-            console.log(dayIndex, day);
+        console.log(response)
+        if (response.error) {
+            alert(response.error.message);
+        } else {
+            document.querySelectorAll(".timesheets").forEach(function (el, i, arr) {
+                el.classList.remove('hide-weekends');
+            });
+            for (var day in response) {
+                var dayIndex = new Date(day + ' EDT').getDay(); // TZ Hack
+                var el = document.querySelector('input.time.regular[data-day="' + dayIndex + '"]');
+                var comments = response[day].comments.join('\n');
+                el.value = Math.round(response[day].seconds / 60 / 60 * 10)/10;
+                el.title = comments;
+                // Trigger onchange for sums
+                var evt = new Event('change');
+                el.dispatchEvent(evt);
+                console.log(dayIndex, day);
+            }
         }
     })
 }
